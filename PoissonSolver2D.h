c7c06d704e0d65a32b14ff261296073f9e7205f9
Dieter <dieter.morgenroth@web.de>
2020-05-29 09:46:56 +0200

    apply correction in laplacian creation

--- PoissonSolver2D.h
+++ PoissonSolver2D.h
@@ -40,6 +40,118 @@ namespace poisson {
 			/*source=*/inTree, /*domain mask=*/inTree, boundaryOp, surfaceNormalTree, state, interrupter, staggered);
 	};
 	
+
+	template<typename BoolTreeType, typename BoundaryOp, typename SurfaceNormalTreeType>
+	inline LaplacianMatrix::Ptr
+		createISLaplacianWithBoundaryConditions2D(
+			const typename BoolTreeType::template ValueConverter<VIndex>::Type& idxTree,
+			const BoolTreeType& interiorMask,
+			const BoundaryOp& boundaryOp,
+			const SurfaceNormalTreeType& surfaceNormalTree,
+			typename math::pcg::Vector<LaplacianMatrix::ValueType>& source,
+			bool staggered)
+	{
+		using VIdxTreeT = typename BoolTreeType::template ValueConverter<VIndex>::Type;
+		using VIdxLeafMgrT = typename tree::LeafManager<const VIdxTreeT>;
+
+		// The number of active voxels is the number of degrees of freedom.
+		const Index64 numDoF = idxTree.activeVoxelCount();
+
+		// Construct the matrix.
+		LaplacianMatrix::Ptr laplacianPtr(
+			new LaplacianMatrix(static_cast<math::pcg::SizeType>(numDoF)));
+		LaplacianMatrix& laplacian = *laplacianPtr;
+
+		// Populate the matrix using a second-order, 7-point CD stencil.
+		VIdxLeafMgrT idxLeafManager(idxTree);
+		if (staggered) {
+			idxLeafManager.foreach(internal::ISStaggeredLaplacianOp<BoolTreeType, BoundaryOp>(
+				laplacian, idxTree, interiorMask, boundaryOp, source));
+		}
+		else {
+			idxLeafManager.foreach(internal::ISLaplacianOp2D<VIdxTreeT, BoundaryOp, SurfaceNormalTreeType>(
+				laplacian, idxTree, boundaryOp, surfaceNormalTree, source));
+		}
+
+		return laplacianPtr;
+	}
+	namespace internal {
+		/// Functor for use with LeafManager::foreach() to populate a sparse %Laplacian matrix
+		template<typename VIdxTreeT, typename BoundaryOp, typename SurfaceNormalTreeType>
+		struct ISLaplacianOp2D
+		{
+			using VIdxLeafT = typename VIdxTreeT::LeafNodeType;
+			using ValueT = LaplacianMatrix::ValueType;
+			using VectorT = typename math::pcg::Vector<ValueT>;
+
+			LaplacianMatrix* laplacian;
+			const VIdxTreeT* idxTree;
+			const BoundaryOp boundaryOp;
+			const SurfaceNormalTreeType* surfaceNormalTree;
+			VectorT* source;
+
+			ISLaplacianOp2D(LaplacianMatrix& m, const VIdxTreeT& idx, const BoundaryOp& op, const SurfaceNormalTreeType& snt, VectorT& src) :
+				laplacian(&m), idxTree(&idx), boundaryOp(op), surfaceNormalTree(&snt), source(&src) {}
+
+			void operator()(const VIdxLeafT& idxLeaf, size_t /*leafIdx*/) const
+			{
+				typename tree::ValueAccessor<const VIdxTreeT> vectorIdx(*idxTree);
+				typename tree::ValueAccessor<const SurfaceNormalTreeType> surfNormalIdx(*surfaceNormalTree);
+				const int kNumOffsets = 6;
+				const Coord ijkOffset[kNumOffsets] = {
+	#if OPENVDB_TOOLS_POISSON_LAPLACIAN_STENCIL == 1
+					Coord(-1,0,0), Coord(1,0,0), Coord(0,-1,0), Coord(0,1,0), Coord(0,0,-1), Coord(0,0,1)
+	#else
+					Coord(-2,0,0), Coord(2,0,0), Coord(0,-2,0), Coord(0,2,0), Coord(0,0,-2), Coord(0,0,2)
+	#endif
+				};
+
+				// For each active voxel in this leaf...
+				for (typename VIdxLeafT::ValueOnCIter it = idxLeaf.cbeginValueOn(); it; ++it) {
+					assert(it.getValue() > -1);
+
+					const Coord ijk = it.getCoord();
+					const math::pcg::SizeType rowNum = static_cast<math::pcg::SizeType>(it.getValue());
+					SurfaceNormalTreeType::ValueType normal = surfNormalIdx.getValue(ijk);
+					normal.normalize();
+					LaplacianMatrix::RowEditor row = laplacian->getRowEditor(rowNum);
+
+					ValueT modifiedDiagonal = 0.f;
+
+					// For each of the neighbors of the voxel at (i,j,k)...
+					for (int dir = 0; dir < kNumOffsets; ++dir) {
+						const Coord neighbor = ijk + ijkOffset[dir];
+						Vec3s offsetDir = Vec3s(ijkOffset[dir].x(), ijkOffset[dir].y(), ijkOffset[dir].z());
+						offsetDir.normalize();
+						const double length_of_projection = normal.length() > 0 ? (offsetDir-offsetDir.projection(normal)).length():1.0f;
+						VIndex column;
+						// For collocated vector grids, the central differencing stencil requires
+						// access to neighbors at a distance of two voxels in each direction
+						// (-x, +x, -y, +y, -z, +z).
+#if OPENVDB_TOOLS_POISSON_LAPLACIAN_STENCIL == 1
+						const bool ijkIsInterior = (vectorIdx.probeValue(neighbor + ijkOffset[dir], column)
+							&& vectorIdx.isValueOn(neighbor));
+#else
+						const bool ijkIsInterior = vectorIdx.probeValue(neighbor, column);
+#endif
+						if (ijkIsInterior) {
+							// If (i,j,k) is sufficiently far away from the exterior,
+							// set its weight to one and adjust the center weight accordingly.
+							row.setValue(column, length_of_projection);
+							modifiedDiagonal -= length_of_projection;
+						}
+						else {
+							// If (i,j,k) is adjacent to or one voxel away from the exterior,
+							// invoke the boundary condition functor.
+							boundaryOp(ijk, neighbor, source->at(rowNum), modifiedDiagonal);
+						}
+					}
+					// Set the (possibly modified) weight for the voxel at (i,j,k).
+					row.setValue(rowNum, modifiedDiagonal);
+				}
+			}
+		};
+	}//namespaces
 	template<
 		typename PreconditionerType,
 		typename TreeType,
@@ -76,8 +188,8 @@ namespace poisson {
 		tools::erodeVoxels(*interiorMask, /*iterations=*/1, tools::NN_FACE);
 
 		// 4. Create the Laplacian matrix.
-		LaplacianMatrix::Ptr laplacian = createISLaplacianWithBoundaryConditions(
-			*idxTree, *interiorMask, boundaryOp, *b, staggered);
+		LaplacianMatrix::Ptr laplacian = createISLaplacianWithBoundaryConditions2D(
+			*idxTree, *interiorMask, boundaryOp, surfaceNormalTree, *b, staggered);
 
 		// 5. Solve the Poisson equation.
 		laplacian->scale(-1.0); // matrix is negative-definite; solve -M x = -b
